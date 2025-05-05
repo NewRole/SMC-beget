6c381c070d7b7bfdf1df5d4b11c969669639b647
NewRole <kotnarcet@gmail.com>
2025-05-05 10:18:20 +1000

    reformat code

--- database/migrations/0001_01_01_000005_add_role_to_users_table.php
+++ database/migrations/0001_01_01_000005_add_role_to_users_table.php
@@ -4,18 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up() {
+    public function up()
+    {
         Schema::table('users', function (Blueprint $table) {
             $table->string('role')->default('user');
         });
     }
 
-    public function down() {
+    public function down()
+    {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('role');
         });
